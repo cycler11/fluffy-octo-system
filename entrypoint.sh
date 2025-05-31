fdbb1e19e365fda50d438fe9ab05a5525e293dbe
Andrey <152691772+cycler11@users.noreply.github.com>
2025-05-31 16:38:55 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+set -e
+
+# Ожидаем доступности PostgreSQL
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "db" -U "user" -d "library" -c '\q'; do
+  >&2 echo "PostgreSQL is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "PostgreSQL is up - executing commands"
+
 # Применяем миграции
 flask db upgrade
 
